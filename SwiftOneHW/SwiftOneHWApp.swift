76e307dec9a185e0bfaa079a5b4dd0f4371657fb
Юрий <145429474+yuriIosDev@users.noreply.github.com>
2024-07-22 21:13:27 +0300

    Initial Commit

---
+++ SwiftOneHW/SwiftOneHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftOneHWApp.swift
+//  SwiftOneHW
+//
+//  Created by Юрий Кузнецов on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftOneHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
