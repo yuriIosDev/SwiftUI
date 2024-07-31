c41d73ff440901b87c6e88089760c976dc3062ea
Юрий <145429474+yuriIosDev@users.noreply.github.com>
2024-07-31 17:21:01 +0300

    Третья домашняя работа по верстке SwiftUI

--- SwiftOneHW/ContentView.swift
+++ SwiftOneHW/ContentView.swift
@@ -2,172 +2,124 @@
 //  ContentView.swift
 //  SwiftOneHW
 //
-//  Created by Юрий Кузнецов on 22.07.2024.
+//  Created by Юрий Кузнецов on 31.07.2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    
     @State var name: String = ""
-    
     var body: some View {
-        ZStack(alignment: .top){
-            VStack(spacing: 40) {
+        ZStack (alignment: .top){
+            VStack (spacing: 63) {
                 HStack {
-                    Button{
-                        
-                    } label: {
-                        Image (.burger)
-                            .resizable()
-                            .frame(width: 37, height: 38)
-                            .padding(.vertical, 3)
-                            .background(.bazaWithe)
+                    HStack (spacing: 45){
+                        Button(action: {}, label: {
+                            Image (.backBTN)
+                                .resizable()
+                                .frame(width: 33, height: 33)
+                        })
+                        VStack (alignment: .leading) {
+                            Text("16 декабря")
+                                .font(.system(size: 30, weight: .bold))
+                            Text("2023 год")
+                                .font(.system(size: 14, weight: .light))
+                        }
+                        .foregroundColor(.white)
                     }
                     Spacer()
-                    Button{
-                            
-                        } label: {
-                            Image (.usertop)
-                                .resizable()
-                                .frame(width: 55, height: 55)
-                                .clipShape(Circle())
-                                .padding(.vertical, 3)
-                                .background(.bazaWithe)
-                        }
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image (.burgerBTN)
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                    })
                 }
-                .padding(.horizontal, 30)
-                
-                ScrollView(.vertical){
-                    VStack (spacing: 56){
-                        
-                    VStack (alignment: .leading, spacing: 14){
-                        Text ("Hello User")
-                            .font(.system(size: 16, weight: .bold))
-                            .foregroundColor(.textUser)
-                        Text ("Find your specialist")
-                            .font(.system(size: 32, weight: .bold))
-                            .foregroundColor(.textUser)
-                            .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .zIndex(1)
+                ScrollView (.vertical, showsIndicators: false) {
+                    VStack (spacing: 32) {
+                        ScrollView (.horizontal, showsIndicators: false) {
+                            HStack (spacing: 30) {
+                                Image (.foyoOne)
+                                    .resizable()
+                                    .frame(width: 282, height: 518)
+                                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                                Image (.fotoTwo)
+                                    .resizable()
+                                    .frame(width: 282, height: 518)
+                                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                            }
+                            .padding(.leading, 30)
+                        }
+                        ScrollView (.horizontal, showsIndicators: false) {
+                            HStack (spacing: 13) {
+                                textFor (text: "Природа")
+                                textFor (text: "Природа")
+                                textFor (text: "Природа")
+                                textFor (text: "Природа")
+                            }
+                            .padding(.leading, 30)
+                        }
                     }
-                        VStack (spacing: 48){
-                            HStack {
-                                ZStack (alignment: .leading){
-                                    TextField("search", text: $name)
-                                        .frame(height: 60)
-                                        .padding(.horizontal, 50)
-                                        .background(.white)
-                                    HStack {
-                                        Image(systemName: "magnifyingglass")
-                                            .padding(.leading, 19)
-                                        
-                                        Spacer()
-                                        Button {
-                                            
-                                        } label: {
-                                            Image(.sam)
-                                                .renderingMode(.template)
-                                                .resizable()
-                                                .frame(width: 21, height: 21)
-                                                .foregroundStyle(.bazaWithe)
-                                            
-                                        }
-                                        .padding(10)
-                                        .background(.tel)
-                                        .clipShape(Circle())
+                    VStack (spacing: 31){
+                        VStack {
+                            VStack (spacing: 14) {
+                                VStack (spacing: 40) {
+                                    Text ("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit")
+                                        .padding(25)
+                                        .foregroundColor(.white)
+                                        .background(.fonText)
+                                        .clipShape(RoundedRectangle(cornerRadius: 30))
+                                    VStack (alignment: .leading, spacing: 7) {
+                                        Text ("16.12.23")
+                                        Text ("Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi")
                                     }
+                                    .padding(25)
+                                    .foregroundColor(.white)
+                                    .background(.fonText)
+                                    .clipShape(RoundedRectangle(cornerRadius: 30))
                                 }
+                                VStack (alignment: .leading, spacing: 7) {
+                                    Text ("16.12.23")
+                                    Text ("Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi")
+                                }
+                                .padding(25)
+                                .foregroundColor(.white)
+                                .background(.fonText)
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
+                            }
+                        }
+                        .padding(.horizontal, 30)
+                        HStack {
+                            TextField ("Добавить комментарий", text: $name)
+                                .padding(.vertical, 15)
+                                .padding(.leading, 25)
+                                .foregroundColor(.fild)
                                 .background(.white)
-                                .clipShape(RoundedRectangle(cornerRadius: 49))
-                            }
-                            
-                            VStack(spacing: 40){
-                                HStack{
-                                    VStack {
-                                        Image(.maps)
-                                            .renderingMode(.template)
-                                            .resizable()
-                                            .frame(width: 42, height: 42)
-                                            .foregroundStyle(.bazaWithe)
-                                        Text("Place")
-                                            .font(.system(size: 16, weight: .regular))
-                                            .foregroundColor(.bazaWithe)
-                                    }
-                                    .padding()
-                                    .frame(width: 112, height: 112)
-                                    .background(.maps)
-                                    .clipShape(RoundedRectangle(cornerRadius: 22))
-                                    Spacer()
-                                    VStack {
-                                        Image(.tel)
-                                            .renderingMode(.template)
-                                            .resizable()
-                                            .frame(width: 42, height: 42)
-                                            .foregroundStyle(.bazaWithe)
-                                        Text("Phone")
-                                            .font(.system(size: 16, weight: .regular))
-                                            .foregroundColor(.bazaWithe)
-                                    }
-                                    .padding()
-                                    .frame(width: 112, height: 112)
-                                    .background(.tel)
-                                    .clipShape(RoundedRectangle(cornerRadius: 22))
-                                    Spacer()
-                                    VStack {
-                                        Image(.user)
-                                            .renderingMode(.template)
-                                            .resizable()
-                                            .frame(width: 42, height: 42)
-                                            .foregroundStyle(.bazaWithe)
-                                        Text("User")
-                                            .font(.system(size: 16, weight: .regular))
-                                            .foregroundColor(.bazaWithe)
-                                    }
-                                    .padding()
-                                    .frame(width: 112, height: 112)
-                                    .background(.user)
-                                    .clipShape(RoundedRectangle(cornerRadius: 22))
-                                    
-                                }
-                                
-
-                                HStack {
-                                    Text ("Top Doctor")
-                                        .font(.system(size: 25, weight: .bold))
-                                    Spacer()
-                                    Button{
-                                            
-                                        } label: {
-                                            Text ("See all")
-                                        }
-                                        .font(.system(size: 14, weight: .regular))
-                                        .foregroundColor(.tel)
-                                    
-                                }
-                                
-                                VStack (spacing: 14) {
-                                    CardView()
-                                    CardView()
-                                    CardView()
-                                }
-                                
-                            }
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
                         }
-                        
+                        .padding(30)
                     }
-                    .padding(.horizontal, 30)
-                    
-                    
                 }
-                .background(.bazaWithe)
             }
+            
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: Alignment(horizontal: .leading, vertical: .top))
-        .background(.bazaWithe)
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity, alignment: Alignment(horizontal: .leading, vertical: .top))
+        .background(.bg)
+            }
+    struct textFor: View {
+        var text: String
+        var body: some View {
+            Text(text)
+                .font(.system(size: 16, weight: .regular))
+                .padding(.horizontal, 22)
+                .padding(.vertical, 8)
+                .foregroundColor(.white)
+                .background(.fonScrText)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+        }
     }
 }
 #Preview {
     ContentView()
 }
-
-//
